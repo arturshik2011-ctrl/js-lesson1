8694a2c2e0c1c3d5e5686544d9dc4a566f958403
arturshik2011-ctrl <arturshik2011@gmail.com>
2025-08-20 03:57:13 -0700

    lesson1

---
+++ index.js
@@ -0,0 +1,68 @@
+var name = "Artur";
+let age = 14;
+const from = "Bishkek"
+
+
+console.log(name)
+console.log(age)
+console.log(from)
+
+// console.log(name)
+// console.log(age)
+// console.log(from)
+
+
+const namee ="Artur";
+const surname ="Anarbaev";
+let agee =15;
+let fromm = "Bishkek"
+
+
+let b = 5;
+let a = 10;
+const resultBA = b + a
+console.log(resultBA)
+
+let c = 5;
+let j = 10;
+const resultCJ = c * j
+console.log(resultCJ)
+
+let v = 5;
+let m = 10;
+const resultVM = v - m
+console.log(resultVM)
+
+let k = 5;
+let p = 10;
+const resultKP = k % p
+console.log(resultKP)
+
+
+let g = 5;
+let y = 10;
+const resultGP = g > y
+console.log(resultGP)
+
+let r = 5;
+let t = 10;
+const resultRT = r < t
+console.log(resultRT)
+
+
+const nameee = "Artur";
+console.log(name)
+
+let width = 10;
+let height = 20;
+const resultWH = width * height
+
+var fisrtname = "Anarbaev";
+var lastname = "Artur"
+console.log(fisrtname, lastname )
+
+
+
+
+
+
